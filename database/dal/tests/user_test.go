2f6e75c97ac929bd9dc09bc804ce8f24185b91b3
akrck02 <aketza.vazquez@gmail.com>
2025-08-07 14:45:34 +0200

    Update user_test.go

--- database/dal/tests/user_test.go
+++ database/dal/tests/user_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/google/uuid"
 )
 
-func UserTest(t *testing.T) {
+func UserCrudTest(t *testing.T) {
 
 	databaseUuid := uuid.NewString()
 	db, err := NewTestDatabase(databaseUuid)
@@ -23,14 +23,20 @@ func UserTest(t *testing.T) {
 		Password: "$P4ssw0rdW3db1128",
 	}
 
-	t.Run("Register validations", func(t *testing.T) { registerValidationErrors(t, db) })
-	t.Run("Register user logic", func(t *testing.T) { expectedUser = registerUser(t, db, expectedUser) })
-	t.Run("Update user mail validation errors", func(t *testing.T) { updateEmailValidateError(t, db) })
-	t.Run("Update user mail logic", func(t *testing.T) { expectedUser = updateUserEmail(t, db, expectedUser) })
+	t.Run("Register validations", func(t *testing.T) { registerValidations(t, db) })
+	t.Run("Register user", func(t *testing.T) { expectedUser = registerUser(t, db, expectedUser) })
+
+	t.Run("Get user validations", func(t *testing.T) { getUserValidations(t, db) })
+
+	t.Run("Update user mail validation errors", func(t *testing.T) { updateEmailValidations(t, db) })
+	t.Run("Update user mail", func(t *testing.T) { expectedUser = updateUserEmail(t, db, expectedUser) })
+
+	t.Run("Update user profile picture validation errors", func(t *testing.T) { updateUserProfilePictureValidations(t, db) })
+	t.Run("Update user profile", func(t *testing.T) { expectedUser = updateUserProfilePicture(t, db, expectedUser) })
 
 }
 
-func registerValidationErrors(t *testing.T, db *sql.DB) {
+func registerValidations(t *testing.T, db *sql.DB) {
 
 	_, err := dal.RegisterUser(nil, nil)
 	AssertVError(t, err, errors.UnexpectedError, "Database connection cannot be empty.")
@@ -71,35 +77,21 @@ func registerUser(t *testing.T, db *sql.DB, user *models.User) *models.User {
 	userId, err := dal.RegisterUser(db, user)
 	AssertVErrorDoesNotExist(t, err)
 
-	t.Run("Get user validations", func(t *testing.T) {
-		_, err := dal.GetUser(db, 0)
-		AssertVError(t, err, errors.InvalidId, "User id must be positive.")
-
-		_, err = dal.GetUser(db, 999)
-		AssertVError(t, err, errors.NotFound, "User not found.")
-	})
-
 	obtainedUser, err := dal.GetUser(db, *userId)
 	AssertVErrorDoesNotExist(t, err)
 	Assert(t, nil != obtainedUser && obtainedUser.Email == obtainedUser.Email, "Expected user and obtained user mismatch")
 	return obtainedUser
 }
 
-func updateUserEmail(t *testing.T, db *sql.DB, user *models.User) *models.User {
+func getUserValidations(t *testing.T, db *sql.DB) {
+	_, err := dal.GetUser(db, 0)
+	AssertVError(t, err, errors.InvalidId, "User id must be positive.")
 
-	newMail := "user-modified@valhalla.org"
-	userId := *&user.Id
-	err := dal.UpdateUserEmail(db, userId, newMail)
-	AssertVErrorDoesNotExist(t, err)
-
-	obtainedUser, err := dal.GetUser(db, userId)
-	AssertVErrorDoesNotExist(t, err)
-
-	Assert(t, user.Email == newMail, "User mail mismatch.")
-	return obtainedUser
+	_, err = dal.GetUser(db, 999)
+	AssertVError(t, err, errors.NotFound, "User not found.")
 }
 
-func updateEmailValidateError(t *testing.T, db *sql.DB) {
+func updateEmailValidations(t *testing.T, db *sql.DB) {
 	err := dal.UpdateUserEmail(nil, 0, "")
 	AssertVError(t, err, errors.UnexpectedError, "Database connection cannot be empty.")
 
@@ -114,4 +106,91 @@ func updateEmailValidateError(t *testing.T, db *sql.DB) {
 
 	err = dal.UpdateUserEmail(db, 1, "u@.org")
 	AssertVError(t, err, errors.InvalidEmail, "mail: missing '@' or angle-addr")
+}
+
+func updateUserEmail(t *testing.T, db *sql.DB, user *models.User) *models.User {
+
+	newMail := "user-modified@valhalla.org"
+	userId := user.Id
+	err := dal.UpdateUserEmail(db, userId, newMail)
+	AssertVErrorDoesNotExist(t, err)
+
+	obtainedUser, err := dal.GetUser(db, userId)
+	AssertVErrorDoesNotExist(t, err)
+
+	Assert(t, user.Email == newMail, "User mail mismatch.")
+	return obtainedUser
+}
+
+func updateUserProfilePictureValidations(t *testing.T, db *sql.DB) {
+	err := dal.UpdateUserProfilePicture(nil, 0, "")
+	AssertVError(t, err, errors.UnexpectedError, "Database connection cannot be empty.")
+
+	err = dal.UpdateUserProfilePicture(db, 0, "")
+	AssertVError(t, err, errors.InvalidId, "User id must be positive.")
+}
+
+func updateUserProfilePicture(t *testing.T, db *sql.DB, user *models.User) *models.User {
+
+	newProfilePic := "my-user-profile-pic.jpg"
+	userId := user.Id
+	err := dal.UpdateUserProfilePicture(db, userId, newProfilePic)
+	AssertVErrorDoesNotExist(t, err)
+
+	obtainedUser, err := dal.GetUser(db, userId)
+	AssertVErrorDoesNotExist(t, err)
+
+	Assert(t, user.ProfilePicture == newProfilePic, "User mail mismatch.")
+	return obtainedUser
+}
+
+func UserLoginTest(t *testing.T) {
+
+	databaseUuid := uuid.NewString()
+	db, err := NewTestDatabase(databaseUuid)
+	AssertVErrorDoesNotExist(t, err)
+	defer db.Close()
+	defer RemoveDatabase(databaseUuid)
+
+	expectedUser := &models.User{
+		Email:    "user@valhalla.org",
+		Password: "$P4ssw0rdW3db1128",
+	}
+
+	userId, err := dal.RegisterUser(db, expectedUser)
+	expectedUser.Id = *userId
+
+	t.Run("Login validation errors", func(t *testing.T) { loginValidation(t, db) })
+	t.Run("Login", func(t *testing.T) { login(t, db) })
+
+	t.Run("Login with auth validation errors", func(t *testing.T) { loginWithAuthValidation(t, db) })
+	t.Run("Login with auth", func(t *testing.T) { loginWithAuth(t, db) })
+
+	t.Run("Validate user account validation errors", func(t *testing.T) { validateUserAccountValidation(t, db) })
+	t.Run("Validate user account", func(t *testing.T) { validateUserAccount(t, db) })
+
+}
+
+func loginValidation(t *testing.T, db *sql.DB) {
+
+}
+
+func login(t *testing.T, db *sql.DB) {
+
+}
+
+func loginWithAuthValidation(t *testing.T, db *sql.DB) {
+
+}
+
+func loginWithAuth(t *testing.T, db *sql.DB) {
+
+}
+
+func validateUserAccountValidation(t *testing.T, db *sql.DB) {
+
+}
+
+func validateUserAccount(t *testing.T, db *sql.DB) {
+
 }
